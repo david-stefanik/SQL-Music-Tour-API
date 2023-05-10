194a960cf46e7db90163886ecf4ce60a9eb8615f
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-05-09 19:07:58 -0700

    sql db connection

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
+// SEQUELIZE CONNECTION
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+const sequelize = new Sequelize(process.env.PG_URI)
 
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
-    })
-})
+    })})
 
 // LISTEN
 app.listen(process.env.PORT, () => {
