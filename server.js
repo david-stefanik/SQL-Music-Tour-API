6dba347fa6464c25290490d99a4598aa207acb41
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-05-09 20:56:39 -0700

    wired migrations

--- server.js
+++ server.js
@@ -10,14 +10,7 @@ const { Sequelize } = require('sequelize')
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-const sequelize = new Sequelize(process.env.PG_URI)
 
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
